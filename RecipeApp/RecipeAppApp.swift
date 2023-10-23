e6e65758816bddd863d5604e9d1f10d2d2c6b883
Shreya Singh <shreyasingh200297.ss@gmail.com>
2023-10-23 13:55:37 -0400

    new project

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Shreya Singh on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
